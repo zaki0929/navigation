904f9df9434a4d106e56a2e32c34dbe00084982c
Kazuya.N <aibotaro@gmail.com>
2017-03-06 18:33:23 +0900

    fix indent

--- amcl/src/amcl/pf/pf.c
+++ amcl/src/amcl/pf/pf.c
@@ -53,7 +53,7 @@ pf_t *pf_alloc(int min_samples, int max_samples,
   pf_t *pf;
   pf_sample_set_t *set;
   pf_sample_t *sample;
-  
+
   srand48(time(NULL));
 
   pf = calloc(1, sizeof(pf_t));
@@ -71,13 +71,13 @@ pf_t *pf_alloc(int min_samples, int max_samples,
   // distrubition will be less than [err].
   pf->pop_err = 0.01;
   pf->pop_z = 3;
-  pf->dist_threshold = 0.5; 
-  
+  pf->dist_threshold = 0.5;
+
   pf->current_set = 0;
   for (j = 0; j < 2; j++)
   {
     set = pf->sets + j;
-      
+
     set->sample_count = max_samples;
     set->samples = calloc(max_samples, sizeof(pf_sample_t));
 
@@ -128,7 +128,7 @@ void pf_set_reset_flag(pf_t *pf, int flag)
 void pf_free(pf_t *pf)
 {
   int i;
-  
+
   for (i = 0; i < 2; i++)
   {
     free(pf->sets[i].clusters);
@@ -136,7 +136,7 @@ void pf_free(pf_t *pf)
     free(pf->sets[i].samples);
   }
   free(pf);
-  
+
   return;
 }
 
@@ -147,16 +147,16 @@ void pf_init(pf_t *pf, pf_vector_t mean, pf_matrix_t cov)
   pf_sample_set_t *set;
   pf_sample_t *sample;
   pf_pdf_gaussian_t *pdf;
-  
+
   set = pf->sets + pf->current_set;
-  
+
   // Create the kd tree for adaptive sampling
   pf_kdtree_clear(set->kdtree);
 
   set->sample_count = pf->max_samples;
 
   pdf = pf_pdf_gaussian_alloc(mean, cov);
-    
+
   // Compute the new sample poses
   for (i = 0; i < set->sample_count; i++)
   {
@@ -171,9 +171,9 @@ void pf_init(pf_t *pf, pf_vector_t mean, pf_matrix_t cov)
   pf->w_slow = pf->w_fast = 0.0;
 
   pf_pdf_gaussian_free(pdf);
-    
+
   // Re-compute cluster statistics
-  pf_cluster_stats(pf, set); 
+  pf_cluster_stats(pf, set);
 
   //set converged to 0
   pf_init_converged(pf);
@@ -211,7 +211,7 @@ void pf_init_model(pf_t *pf, pf_init_model_fn_t init_fn, void *init_data)
 
   // Re-compute cluster statistics
   pf_cluster_stats(pf, set);
-  
+
   //set converged to 0
   pf_init_converged(pf);
 
@@ -221,8 +221,8 @@ void pf_init_model(pf_t *pf, pf_init_model_fn_t init_fn, void *init_data)
 void pf_init_converged(pf_t *pf){
   pf_sample_set_t *set;
   set = pf->sets + pf->current_set;
-  set->converged = 0; 
-  pf->converged = 0; 
+  set->converged = 0;
+  pf->converged = 0;
 }
 
 int pf_update_converged(pf_t *pf)
@@ -243,19 +243,19 @@ int pf_update_converged(pf_t *pf)
   }
   mean_x /= set->sample_count;
   mean_y /= set->sample_count;
-  
+
   for (i = 0; i < set->sample_count; i++){
     sample = set->samples + i;
-    if(fabs(sample->pose.v[0] - mean_x) > pf->dist_threshold || 
+    if(fabs(sample->pose.v[0] - mean_x) > pf->dist_threshold ||
        fabs(sample->pose.v[1] - mean_y) > pf->dist_threshold){
-      set->converged = 0; 
-      pf->converged = 0; 
+      set->converged = 0;
+      pf->converged = 0;
       return 0;
     }
   }
-  set->converged = 1; 
-  pf->converged = 1; 
-  return 1; 
+  set->converged = 1;
+  pf->converged = 1;
+  return 1;
 }
 
 // Update the filter with some new action
@@ -300,7 +300,7 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
     }
     w_v = ((w_sumv) - (w_sum * w_sum / set->sample_count)) / set->sample_count;
 
-    // Update running averages of likelihood of samples (Prob Rob p258)    
+    // Update running averages of likelihood of samples (Prob Rob p258)
     w_avg = w_sum / set->sample_count;
     if(pf->w_slow == 0.0)
       pf->w_slow = w_avg;
@@ -310,10 +310,8 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
       pf->w_fast = w_avg;
     else
       pf->w_fast += pf->alpha_fast * (w_avg - pf->w_fast);
-    //printf("w_avg: %e slow: %e fast: %e\n", 
+    //printf("w_avg: %e slow: %e fast: %e\n",
            //w_avg, pf->w_slow, pf->w_fast);
-
-
 /*----------------------------------------------------------------------------------*/
     double beta = 1.0 - (w_sum / pf->alpha);
 //	printf("w_sum : %e, w_v : %e\n", w_sum, w_v);
@@ -328,9 +326,9 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
       int reset_limit = 0, reset_count = 0;
 
       // printf("kidnapped\n");
-      
+
       pf_kdtree_clear(set->kdtree);
-      
+
       for (i = 0; i < set->sample_count; i++)
       {
         sample = set->samples + i;
@@ -345,7 +343,7 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
       x_v = (x_sumv - (x_sum * x_sum / set->sample_count)) / set->sample_count;
       y_v = (y_sumv - (y_sum * y_sum / set->sample_count)) / set->sample_count;
       theta_v = (theta_sumv - (theta_sum * theta_sum / set->sample_count)) / set->sample_count;
-      
+
       //分散の制限(これがないとエラーを起こす), オーバーフロー防止
       if(x_v >= v_limit){
         x_v = v_limit;
@@ -365,8 +363,8 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
       if(theta_v <= -M_PI/2){
         theta_v = -M_PI/2;
       }
-      
-        reset_limit = ((int)x_v + (int)y_v) / 2;
+
+      reset_limit = ((int)x_v + (int)y_v) / 2;
 
       //分散の分だけ広げる
       if(reset_count >= reset_limit){
@@ -376,8 +374,7 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
           sample->pose.v[1] += (drand48() * 4 * y_v) - (2 * y_v);
           sample->pose.v[2] += (drand48() * 2 * theta_v) - (1 * theta_v);
           sample->weight = 1.0 / set->sample_count;
-          }
-
+        }
         reset_count = 0;
         total = (*sensor_fn) (sensor_data, set);
       }
@@ -396,11 +393,11 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
     // Handle zero total
     for (i = 0; i < set->sample_count; i++)
     {
-      sample = set->samples + i; 
+      sample = set->samples + i;
       sample->weight = 1.0 / set->sample_count;
     }
   }
-  
+
   return;
 }
 
@@ -433,7 +430,7 @@ void pf_update_resample(pf_t *pf)
 
   // Create the kd tree for adaptive sampling
   pf_kdtree_clear(set_b->kdtree);
-  
+
   // Draw samples from set a to create set b.
   total = 0;
   set_b->sample_count = 0;
@@ -513,7 +510,7 @@ void pf_update_resample(pf_t *pf)
     if (set_b->sample_count > pf_resample_limit(pf, set_b->kdtree->leaf_count))
       break;
   }
-  
+
   // Reset averages, to avoid spiraling off into complete randomness.
   if(w_diff > 0.0)
     pf->w_slow = pf->w_fast = 0.0;
@@ -526,12 +523,12 @@ void pf_update_resample(pf_t *pf)
     sample_b = set_b->samples + i;
     sample_b->weight /= total;
   }
-  
+
   // Re-compute cluster statistics
   pf_cluster_stats(pf, set_b);
 
   // Use the newly created sample set
-  pf->current_set = (pf->current_set + 1) % 2; 
+  pf->current_set = (pf->current_set + 1) % 2;
 
   pf_update_converged(pf);
 
@@ -561,7 +558,7 @@ int pf_resample_limit(pf_t *pf, int k)
     return pf->min_samples;
   if (n > pf->max_samples)
     return pf->max_samples;
-  
+
   return n;
 }
 
@@ -572,7 +569,7 @@ void pf_cluster_stats(pf_t *pf, pf_sample_set_t *set)
   int i, j, k, cidx;
   pf_sample_t *sample;
   pf_cluster_t *cluster;
-  
+
   // Workspace
   double m[4], c[2][2];
   size_t count;
@@ -580,7 +577,7 @@ void pf_cluster_stats(pf_t *pf, pf_sample_set_t *set)
 
   // Cluster the samples
   pf_kdtree_cluster(set->kdtree);
-  
+
   // Initialize cluster stats
   set->cluster_count = 0;
 
@@ -609,7 +606,7 @@ void pf_cluster_stats(pf_t *pf, pf_sample_set_t *set)
   for (j = 0; j < 2; j++)
     for (k = 0; k < 2; k++)
       c[j][k] = 0.0;
-  
+
   // Compute cluster stats
   for (i = 0; i < set->sample_count; i++)
   {
@@ -624,7 +621,7 @@ void pf_cluster_stats(pf_t *pf, pf_sample_set_t *set)
       continue;
     if (cidx + 1 > set->cluster_count)
       set->cluster_count = cidx + 1;
-    
+
     cluster = set->clusters + cidx;
 
     cluster->count += 1;
@@ -657,7 +654,7 @@ void pf_cluster_stats(pf_t *pf, pf_sample_set_t *set)
   for (i = 0; i < set->cluster_count; i++)
   {
     cluster = set->clusters + i;
-        
+
     cluster->mean.v[0] = cluster->m[0] / cluster->weight;
     cluster->mean.v[1] = cluster->m[1] / cluster->weight;
     cluster->mean.v[2] = atan2(cluster->m[3], cluster->m[2]);
@@ -705,14 +702,14 @@ void pf_get_cep_stats(pf_t *pf, pf_vector_t *mean, double *var)
   double mn, mx, my, mrr;
   pf_sample_set_t *set;
   pf_sample_t *sample;
-  
+
   set = pf->sets + pf->current_set;
 
   mn = 0.0;
   mx = 0.0;
   my = 0.0;
   mrr = 0.0;
-  
+
   for (i = 0; i < set->sample_count; i++)
   {
     sample = set->samples + i;
@@ -753,5 +750,3 @@ int pf_get_cluster_stats(pf_t *pf, int clabel, double *weight,
 
   return 1;
 }
-
-
